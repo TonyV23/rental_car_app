e643123ef2ded72b2fbaf3edce57f966515a31c4
viper23 <htonnycarlos@gmail.com>
2025-01-13 17:41:50 +0200

    added a gesture detector widget for redirection feature

--- lib/presentation/widgets/car_card.dart
+++ lib/presentation/widgets/car_card.dart
@@ -7,54 +7,56 @@ class CarCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-      padding: EdgeInsets.all(20),
-      decoration: BoxDecoration(
-          color: Color(0xffF3F3F3),
-          borderRadius: BorderRadius.circular(20),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black12,
-              blurRadius: 10,
-              spreadRadius: 5,
+    return GestureDetector(
+      child: Container(
+        margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+        padding: EdgeInsets.all(20),
+        decoration: BoxDecoration(
+            color: Color(0xffF3F3F3),
+            borderRadius: BorderRadius.circular(20),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black12,
+                blurRadius: 10,
+                spreadRadius: 5,
+              )
+            ]),
+        child: Column(
+          children: [
+            Image.asset(
+              "assets/car_image.png",
+              height: 120,
+            ),
+            Text(
+              car.model,
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Image.asset("assets/gps.png"),
+                    Text('${car.distance.toStringAsFixed(0)}km')
+                  ],
+                ),
+                Row(
+                  children: [
+                    Image.asset("assets/pump.png"),
+                    Text('${car.fuelCapacity.toStringAsFixed(0)}ltr')
+                  ],
+                ),
+              ],
+            ),
+            Text(
+              '\$${car.pricePerHour.toStringAsFixed(2)}/hr',
+              style: TextStyle(fontSize: 16),
             )
-          ]),
-      child: Column(
-        children: [
-          Image.asset(
-            "assets/car_image.png",
-            height: 120,
-          ),
-          Text(
-            car.model,
-            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Row(
-                children: [
-                  Image.asset("assets/gps.png"),
-                  Text('${car.distance.toStringAsFixed(0)}km')
-                ],
-              ),
-              Row(
-                children: [
-                  Image.asset("assets/pump.png"),
-                  Text('${car.fuelCapacity.toStringAsFixed(0)}ltr')
-                ],
-              ),
-            ],
-          ),
-          Text(
-            '\$${car.pricePerHour.toStringAsFixed(2)}/hr',
-            style: TextStyle(fontSize: 16),
-          )
-        ],
+          ],
+        ),
       ),
     );
   }
